6547a57a0f3f362a65d3f0b3e3e9d4a261797a9f
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-07 21:25:34 +0200

    add:config

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import CardsList from "./components/CardsList.vue";
+import AppSearchBar from "./components/AppSearchBar.vue";
+
+import { CalculationOperation } from "sass";
 
 export default {
   components: {
     AppHeader,
     CardsList,
+    AppSearchBar,
   },
   data() {
     return {
@@ -25,12 +29,31 @@ export default {
         this.cardsArray = resp.data.results;
         console.log(this.cardsArray);
       });
+      
+  },
+  methods: {
+    typeStatus(){
+      const  paramsObj= {status:""};
+      if (this.store.status !== "all"){
+        paramsObj.status=this.store.status
+      }
+     axios
+      .get("https://rickandmortyapi.com/api/character", {
+       params: paramsObj
+      })
+      .then((resp) => {
+        this.cardsArray = resp.data.results;
+        console.log(this.cardsArray);
+      });
+    }
+    
   },
 };
 </script>
 
 <template>
   <AppHeader />
+  <AppSearchBar @filter="typeStatus"/>
   <CardsList :cardsArray="cardsArray" />
 </template>
 
---
+++ src/components/AppSearchBar.vue
@@ -0,0 +1,27 @@
+<script>
+import { store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+      status: ["all","alive", "dead", "unknown"],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container py-4">
+    <div class="d-flex justify-content-between">
+      <div>
+        <select @change="$emit('filter')" v-model="store.selectedStatus">
+          <option :value="curStatus" v-for="curStatus in status">
+            {{ curStatus }}
+          </option>
+        </select>
+      </div>
+
+      <p>Mostrati {{ store.cardsList.length }} elementi</p>
+    </div>
+  </div>
+</template>
